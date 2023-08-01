c0cb0cb233550f76a5143a2445d4d32751dfecf3
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-08-01 11:08:38 +0800

    创建java项目

---
+++ PersonnelSalaryManagement/src/main/java/com/tyza66/personnelsalarymanagement/PersonnelSalaryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tyza66.personnelsalarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelSalaryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelSalaryManagementApplication.class, args);
+	}
+
+}
---
+++ PersonnelSalaryManagement/src/test/java/com/tyza66/personnelsalarymanagement/PersonnelSalaryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyza66.personnelsalarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelSalaryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
