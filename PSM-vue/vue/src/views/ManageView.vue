4268fae191ec2add8987d33f7112f641a3e8d682
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-08-01 15:01:17 +0800

    更新

---
+++ PSM-vue/vue/src/views/ManageView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home" style="width: 100%;padding: 10px">
+    
+  </div>
+</template>
+
+<script>
+import { request } from '@/utils/request';
+
+export default {
+  name: 'ManageView',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+
+  }
+}
+</script>
--- PersonnelSalaryManagement/src/main/java/com/tyza66/personnelsalarymanagement/controller/AdminController.java
+++ PersonnelSalaryManagement/src/main/java/com/tyza66/personnelsalarymanagement/controller/AdminController.java
@@ -9,10 +9,7 @@ import com.tyza66.personnelsalarymanagement.service.AdminService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Author: tyza66
@@ -69,5 +66,18 @@ public class AdminController {
         return obj;
     }
 
-
+    @ApiOperation("检查当前登录状态")
+    @GetMapping("/check")
+    public JSON check() {
+        JSONObject obj = JSONUtil.createObj();
+        if (StpUtil.isLogin()) {
+            obj.put("code", 200);
+            obj.put("msg", "已登录");
+            obj.put("username", StpUtil.getSession().get("username"));
+        } else {
+            obj.put("code", 201);
+            obj.put("msg", "未登录");
+        }
+        return obj;
+    }
 }
